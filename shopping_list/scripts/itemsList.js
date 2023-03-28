2cfd9f33617a4347caf7f13f98818ed4aa9b0bc9
Michael Moser <michaelmoser90@outlook.com>
2023-03-28 21:00:56 +0200

    shopping-list project

---
+++ shopping_list/scripts/itemsList.js
@@ -0,0 +1,17 @@
+let items = [
+	{id: '1',
+	img: 'https://web.getbring.com/assets/images/items/aufschnitt.png',
+	title: 'Aufschnitt',
+	amount: '',
+	isInShoppingList: false},
+	{i: '2',
+	img: 'https://web.getbring.com/assets/images/items/speck.png',
+	title: 'Speck',
+	amount: '',
+	isInShoppingList: false},
+	{id: '3',
+	img: 'https://web.getbring.com/assets/images/items/milch.png',
+	title: 'Milch',
+	amount: '',
+	isInShoppingList: false},
+];
---
+++ shopping_list/scripts/main.js
@@ -0,0 +1,17 @@
+// model
+
+// view
+const render = () => {
+	/* 
+	CREATE ELEMENT TO RENDER
+	1.. button class=shopping-button
+		give this button an ID
+	2.. img class = item-image
+	2.. div class = button-middlesection
+	3.. div class = item-name
+	3.. div class = item-amount
+	2.. img class = item-settings
+	*/
+}
+
+// controller
